9e4718640d91875fd0469ad70ba0696a3f621b88
Aleyna Tombaş <aleynatombas04@gmail.com>
2026-02-07 17:07:36 +0300

    feat: update core DTOs

--- backend/RusWallet.Core/DTOs/Auth/AuthResponseDto.cs
+++ backend/RusWallet.Core/DTOs/Auth/AuthResponseDto.cs
@@ -3,7 +3,8 @@ namespace RusWallet.Core.DTOs.Auth
     public class AuthResponseDto
     {
         public int UserId { get; set; }
-        public string Name { get; set; }
+         public string FirstName {get; set;}
+        public string LastName{get; set;} 
         public string Email { get; set; }
         public string Token { get; set; }
     }
--- backend/RusWallet.Core/DTOs/Auth/RegisterRequestDto.cs
+++ backend/RusWallet.Core/DTOs/Auth/RegisterRequestDto.cs
@@ -2,7 +2,9 @@ namespace RusWallet.Core.DTOs.Auth
 {
     public class RegisterRequestDto
     {
-        public string Name { get; set; }
+         public string FirstName {get; set;}
+        public string LastName{get; set;} 
+        public string PhoneNumber{get; set;}
         public string Email { get; set; }
         public string Password { get; set; }
     }
